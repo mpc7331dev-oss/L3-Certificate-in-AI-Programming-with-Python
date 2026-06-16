3fe8c4596b1d8c0948f5caf55f6ddb9354439b72
mpc7331dev-oss <mpc7331dev@gmail.com>
2026-06-16 11:20:11 +0100

    Add python file Calculator via upload
    This is a simple python file which allows a user to enter two numbers and choose the mathematical operator (+, -, / *) to use for these numbers.

---
+++ calculator.py
@@ -0,0 +1,33 @@
+#Calculator: asks for two numbers and then performs the necessary operations
+
+numbers_needed = []
+
+def machine():
+
+    num1 = float(input("What is your first number? "))
+    num2 = float(input("What is your second number? "))
+    
+    op = input("""Which operator do you want?
+
+add?
+subtract?
+times?
+divide?
+
+""")
+    if op == "add":
+        return print("The answer is: ",num1+num2)
+    elif op == "subtract":
+        return print("The answer is: ",num1-num2)
+    elif op == "times":
+        return print("The answer is: ",num1*num2)
+    elif op == "divide":
+        try:
+            return print("The answer is: ",num1/num2)
+        except:
+            num2 = 0
+            print("You cannot divide by zero!")
+    else:
+        print("Invalid selection!")
+        
+machine()
